8d8ce1cd979fa9b53d43b3227554f02e2c56e671
haruza123 <haruzakatana@gmail.com>
2023-03-11 19:44:30 +0700

    Create grounded.cs

---
+++ grounded.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+            if (player != null)
+            {
+                player.isGrounded = true;
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+            if (player != null)
+            {
+                player.isGrounded = false;
+            }
+        }
+    }
+}
